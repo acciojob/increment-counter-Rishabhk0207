f289773461788f1c2822c2975447c583dbf9ec25
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-01-25 20:31:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let count = document.querySelector("[counter]");
+let btn = document.querySelector("[incrementBtn]");
+
+let add =0;
+btn.addEventListener("click",(e)=>{
+	alert(add);
+	count.innerText = ++add;
+})
